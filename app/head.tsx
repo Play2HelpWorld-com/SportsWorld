5aa42fa192410cdc299f394c183d117ff568e56c
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2025-01-28 21:09:47 +0600

    project setup

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Play2MagicWorld | Play for Free Donation</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Built by bahadur" />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
