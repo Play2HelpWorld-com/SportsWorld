7dde678730f2185567a4a2bfa31bc81209b967b3
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2025-02-04 01:18:05 +0600

    all content change

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -64,16 +64,16 @@ const About = () => {
                 <span className="mb-4 mr-4 inline-flex rounded-full bg-meta px-4.5 py-1 text-metatitle uppercase text-white ">
                   New
                 </span>{" "}
-                Play2MagicWorld Features
+                Sports World Features
               </span>
               <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
                 You can have more
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
-                  with Play2MagicWorld
+                  with Sports World
                 </span>
               </h2>
               <p>
-              Play2MagicWorld is an innovative platform that lets you earn while you play! Enjoy exciting games, collect rewards, and turn them into real money.
+              Sports World is an innovative platform that lets you earn while you play! Enjoy exciting games, collect rewards, and turn them into real money.
               </p>
 
               <div className="mt-7.5 flex items-center gap-5">
@@ -137,13 +137,13 @@ const About = () => {
                 Charities
               </h4>
               <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
-              Play2MagicWorld 
+              Sports World 
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
                   &nbsp;
                 </span>
               </h2>
               <p>
-              Play2MagicWorld is an innovative platform that lets you earn while you play! Enjoy exciting games, collect rewards, and turn them into real money.
+              Sports World is an innovative platform that lets you earn while you play! Enjoy exciting games, collect rewards, and turn them into real money.
               </p>
               <div>
                 <a
--- components/Testimonial/index.tsx
+++ components/Testimonial/index.tsx
@@ -22,7 +22,7 @@ const Testimonial = () => {
               headerInfo={{
                 title: `TESTIMONIALS`,
                 subtitle: `User’s Testimonials`,
-                description: `Play2MagicWorld is a free gaming platform where users can play games to earn coupons. These coupons can be converted into real money, which users can either withdraw or donate to charitable organizations, blending entertainment with a meaningful cause.`,
+                description: `Sports World is a free gaming platform where users can play games to earn coupons. These coupons can be converted into real money, which users can either withdraw or donate to charitable organizations, blending entertainment with a meaningful cause.`,
               }}
             />
           </div>
